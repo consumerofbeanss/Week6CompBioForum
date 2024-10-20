06b71c7a6bf87263de4434d2d129585f07326dac
consumerofbeanss <114371990+consumerofbeanss@users.noreply.github.com>
2024-10-20 18:16:24 +0700

    Add files via upload

---
+++ Week6.py
@@ -0,0 +1,17 @@
+from Bio import AlignIO
+from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
+from Bio import Phylo
+
+input_file = 'sample_sequences.fasta'
+alignment = AlignIO.read(input_file, 'fasta')
+
+calculator = DistanceCalculator('identity')  # using identity model for simple distance calculation
+distance_matrix = calculator.get_distance(alignment)
+print('Distance Matrix:', distance_matrix)
+
+constructor = DistanceTreeConstructor(calculator, method='nj')
+phylo_tree = constructor.build_tree(alignment)
+phylo_tree.rooted = True
+
+Phylo.draw(phylo_tree)
+Phylo.write(phylo_tree, 'phylogenetic_tree.xml', 'phyloxml')
